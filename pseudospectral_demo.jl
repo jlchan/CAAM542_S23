b32a2281f3b41cf076d36b5af4c8c2d8828f81d9
Jesse Chan <jesse.chan@rice.edu>
2023-01-13 11:57:22 -0600

    update pseudospectral demo

--- pseudospectral_demo.jl
+++ pseudospectral_demo.jl
@@ -1,17 +1,18 @@
 using OrdinaryDiffEq
 using NodesAndModes
 
+# polynomial degree
 N = 25
 
 # Chebyshev nodes: "good" points for interpolation
 x = [-cos(k * pi / N) for k in 0:N] 
 
-# create differentiation matrix
+# create the nodal differentiation matrix
 VDM, dVdx = basis(Line(), N, x)
-D = dVdx / VDM
+D = dVdx / VDM # note that A / B = A * inv(B)
 
 function rhs!(du, u, parameters, t)
-    (; D) = parameters
+    D = parameters.D
 
     # a hacky way to enforce periodic boundary conditions
     u[1] = u[end] 
@@ -19,19 +20,30 @@ function rhs!(du, u, parameters, t)
     du .= -D * u
 end
 
+# u0(x) = sin(pi * x)
 u0(x) = exp(-25 * x^2)
+# u0(x) = abs(x) < 0.5
+
 u = u0.(x)
 params = (; D)
+tspan = (0.0, 2.0)
 ode = ODEProblem(rhs!, u, tspan, params)
-sol = solve(ode, Tsit5(), dt = 1e-5, saveat=LinRange(tspan[1], tspan[2], 50))
+sol = solve(ode, RK4(), saveat=LinRange(tspan[1], tspan[2], 50))
 
-# interpolation to equispaced plotting nodes
-xp = LinRange(-1, 1, 100)
-Vp = vandermonde(Line(), N, xp) / VDM
+Linf_error = maximum(abs.(sol.u[end] - u0.(x)))
+plot(x, sol.u[end], label = "Pseudospectral solution", marker=:dot)
+plot!(x, u0.(x), label = "Exact solution")
+title!("Error = $Linf_error")
 
-@gif for i in eachindex(sol.u)
-    t = sol.t[i]
-    plot(xp, Vp * sol.u[i])
-    plot!(xp, u0.(xp))
-    plot!(ylims = extrema(u0.(x)) .+ (-0.5, 0.5), leg=false, title="Time = $t")
-end
+# # interpolation to equispaced plotting nodes
+# xp = LinRange(-1, 1, 100)
+# Vp = vandermonde(Line(), N, xp) / VDM
+
+# @show Linf_error = maximum(abs.(Vp * sol.u[end] - u0.(xp)))
+
+# @gif for i in eachindex(sol.u)
+#     t = sol.t[i]
+#     plot(xp, Vp * sol.u[i])
+#     plot!(xp, u0.(xp))
+#     plot!(ylims = extrema(u0.(x)) .+ (-0.5, 0.5), leg=false, title="Time = $t")
+# end
