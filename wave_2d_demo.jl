293440ecb8e8945977fd72188adfd6cae53e0916
Jesse Chan <jesse.chan@rice.edu>
2023-02-07 09:24:56 -0600

    revert from view

--- wave_2d_demo.jl
+++ wave_2d_demo.jl
@@ -7,28 +7,31 @@ num_elements = 16
 
 rd = RefElemData(Tri(), N)
 md = MeshData(uniform_mesh(Tri(), num_elements), rd)
-md = make_periodic(md)
+# md = make_periodic(md)
 
 # strong form
 function rhs!(du, u, parameters, t)
     (; rd, md) = parameters 
     (; Vf, Dr, Ds, LIFT) = rd
-    (; rxJ, sxJ, ryJ, syJ, nx, ny, J, Jf, mapP) = md
+    (; rxJ, sxJ, ryJ, syJ, J, nx, ny, Jf, mapP) = md
 
     p, u, v = view(u, :, :, 1), view(u, :, :, 2), view(u, :, :, 3)
 
     pM, uM, vM = Vf * p, Vf * u, Vf * v
-    pP, uP, vP = view(pM, mapP), view(uM, mapP), view(vM, mapP)
+    pP, uP, vP = pM[mapP], uM[mapP], vM[mapP]
+    pP[md.mapB] .= -pP[md.mapB]
 
     p_flux = @. 0.5 * ((uP - uM) * nx + (vP - vM) * ny) - 0.5 * (pP - pM)
-    u_flux = @. 0.5 * (pP - pM) * nx - 0.5 * (uP - uM)
-    v_flux = @. 0.5 * (pP - pM) * ny - 0.5 * (vP - vM)
+    u_flux = @. 0.5 * (pP - pM) * nx                    - 0.5 * (uP - uM)
+    v_flux = @. 0.5 * (pP - pM) * ny                    - 0.5 * (vP - vM)
 
     dpdr, dpds = Dr * p, Ds * p
     dpdxJ = rxJ .* dpdr + sxJ .* dpds
     dpdyJ = ryJ .* dpdr + syJ .* dpds
+
     dudxJ = rxJ .* (Dr * u) + sxJ .* (Ds * u)
     dvdyJ = ryJ .* (Dr * v) + syJ .* (Ds * v)
+
     du[:, :, 1] .= -(dudxJ + dvdyJ + LIFT * (p_flux .* Jf)) ./ J
     du[:, :, 2] .= -(dpdxJ         + LIFT * (u_flux .* Jf)) ./ J
     du[:, :, 3] .= -(dpdyJ         + LIFT * (v_flux .* Jf)) ./ J
@@ -41,15 +44,15 @@ u = zeros(rd.Np, md.num_elements, 3)
 u[:, :, 1] .= p0.(x, y)
 
 params = (; rd, md)
-tspan = (0.0, 2)
+tspan = (0.0, 4)
 ode = ODEProblem(rhs!, u, tspan, params)
 
-include("alive.jl") # defines AliveCallback for monitoring progress
+include("alive.jl") # defines an AliveCallback to monitor solve progress
 sol = solve(ode, RK4(), saveat=LinRange(tspan[1], tspan[2], 50), 
             callback=AliveCallback(alive_interval=10))
 
-# scatter(vec(rd.Vp * x), vec(rd.Vp * y), zcolor=vec(rd.Vp * sol.u[end]), 
-#         markersize=2, markerstrokewidth=0, legend=false)
+scatter(vec(rd.Vp * x), vec(rd.Vp * y), zcolor=vec(rd.Vp * sol.u[end][:,:,1]), 
+        markersize=2, markerstrokewidth=0, legend=false, ratio=1)
 
 xp, yp = vec(rd.Vp * x), vec(rd.Vp * y)        
 @gif for i in eachindex(sol.u)    
